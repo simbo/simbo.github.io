9d508b452a74cccea2cd7cc7ebc7e6c62ec1cc58
Simon Lepel <simbo@simbo.de>
2023-11-13 16:26:36 +0100

    remove remains of message bus

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,16 +1,6 @@
 /// <reference types="vite/client" />
 
-import { Subscriptions } from './lib/message-bus';
-
 declare const SITE_VERSION: string;
 declare const SITE_LAST_BUILD: string;
 declare const SITE_IS_PROD: boolean;
 declare const SITE_IS_DEV: boolean;
-
-declare global {
-  interface Window {
-    simbo?: {
-      messageBusSubscriptions?: Subscriptions;
-    };
-  }
-}

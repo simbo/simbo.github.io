a8bced7695ef816611b96322b00db1808d204188
Simon Lepel <simbo@simbo.de>
2023-11-17 01:07:08 +0100

    add commands to maximize/minimize terminal view

--- src/components/command-prompt/command-handlers/maximize.ts
+++ src/components/command-prompt/command-handlers/maximize.ts
@@ -1,5 +1,15 @@
 import { CommandHandler } from '../command-prompt.types';
 
-const maximize: CommandHandler = async () => {};
+const maximize: CommandHandler = async prompt => {
+  if (window.matchMedia('(max-width: 550px)').matches) {
+    throw new Error('sorry, the terminal view can not be resized on small viewports');
+  }
+  if (document.documentElement.classList.contains('terminal-maximized')) {
+    prompt.outputText('terminal view is already maximized');
+  } else {
+    document.documentElement.classList.add('terminal-maximized');
+    prompt.outputText('terminal view maximized');
+  }
+};
 
 export default maximize;
--- src/components/command-prompt/command-handlers/minimize.ts
+++ src/components/command-prompt/command-handlers/minimize.ts
@@ -1,5 +1,15 @@
 import { CommandHandler } from '../command-prompt.types';
 
-const minimize: CommandHandler = async () => {};
+const minimize: CommandHandler = async prompt => {
+  if (window.matchMedia('(max-width: 550px)').matches) {
+    throw new Error('sorry, the terminal view can not be resized on small viewports');
+  }
+  if (document.documentElement.classList.contains('terminal-maximized')) {
+    document.documentElement.classList.remove('terminal-maximized');
+    prompt.outputText('terminal view minimized');
+  } else {
+    prompt.outputText('terminal view is already minimized');
+  }
+};
 
 export default minimize;

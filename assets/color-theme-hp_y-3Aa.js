4161adfe850db17d35c8cfb6b7748ebf8d51f4f3
simbo <simbo@simbo.de>
2023-11-17 09:38:52 +0000

    Release v3.1.5 from main @ 3cb4dcc911bef0c68e7efad3644ee0f868a859dd

---
+++ assets/color-theme-hp_y-3Aa.js
@@ -0,0 +1,2 @@
+import{C as e}from"./index-2owh-gj-.js";const m=async(t,{inputs:r})=>{const o=r.at(-1)?.toLocaleLowerCase();o==="toggle"?e.toggle():e.theme=o,t.outputText(`current color theme: ${e.theme}`)};export{m as default};
+//# sourceMappingURL=color-theme-hp_y-3Aa.js.map

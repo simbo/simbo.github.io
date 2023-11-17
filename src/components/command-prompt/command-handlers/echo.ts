369aae221d1650188fb7fad5d2d5b8823693f902
Simon Lepel <simbo@simbo.de>
2023-11-17 02:21:41 +0100

    remove console.log

--- src/components/command-prompt/command-handlers/echo.ts
+++ src/components/command-prompt/command-handlers/echo.ts
@@ -3,7 +3,6 @@ import { CommandHandler } from '../command-prompt.types';
 
 const echo: CommandHandler = async (prompt, parameters) => {
   const partials = parameters.inputs;
-  console.log(parameters);
   prompt.outputText(`${partials.map(partial => sanitizeText(partial)).join(' ')}`);
 };
 

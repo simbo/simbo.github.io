f3179fd90383fff41f00c903e8145d01b469b228
Simon Lepel <simbo@simbo.de>
2023-11-17 01:08:37 +0100

    fix naming of module scope constant

--- src/components/command-prompt/command-handlers/hello.ts
+++ src/components/command-prompt/command-handlers/hello.ts
@@ -2,10 +2,10 @@ import { CommandHandler } from '../command-prompt.types';
 
 const HELLO = ['Hello', 'Hallo', 'Servus', 'Salut', 'Ciao', 'Hej', 'Hola', 'Cześć', 'Olá'];
 
-let runCount = 0;
+let RUN_COUNT = 0;
 
 const hello: CommandHandler = async prompt => {
-  const index = runCount++ === 0 ? 0 : Math.round(Math.random() * (HELLO.length - 1));
+  const index = RUN_COUNT++ === 0 ? 0 : Math.round(Math.random() * (HELLO.length - 1));
   prompt.outputText(`${HELLO[index]}!`);
 };
 

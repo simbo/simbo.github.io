003427a2e5b47321448e0a5bb0f450462050c5a7
simbo <simbo@simbo.de>
2023-11-17 12:24:07 +0000

    Release v3.1.6 from main @ bf50ddb875f49f3e3bc228bdbae8274df68a87f8

---
+++ assets/clear-rl-NzIU3.js
@@ -0,0 +1,2 @@
+const a=async(t,{options:e,inputs:l})=>{t.clearOutput(),(e.a||e.all||l.includes("all"))&&document.querySelector("typed-text").resetTyping()};export{a as default};
+//# sourceMappingURL=clear-rl-NzIU3.js.map
---
+++ assets/color-theme-bKpSlUQv.js
@@ -0,0 +1,2 @@
+import{C as e}from"./index-7IZKcxRS.js";const m=async(t,{inputs:r})=>{const o=r.at(-1)?.toLocaleLowerCase();o==="toggle"?e.toggle():e.theme=o,t.outputText(`current color theme: ${e.theme}`)};export{m as default};
+//# sourceMappingURL=color-theme-bKpSlUQv.js.map
